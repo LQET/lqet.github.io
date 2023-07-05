c446388c1690b06288ea5b2b46226a70e41a0e43
LQET <137189169+LQET@users.noreply.github.com>
2023-07-05 15:49:35 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBl8VK9xXMVeAQDH4Pvk5YDfmkSHJugI4E",
+  authDomain: "lqet-9aa46.firebaseapp.com",
+  projectId: "lqet-9aa46",
+  storageBucket: "lqet-9aa46.appspot.com",
+  messagingSenderId: "850395072418",
+  appId: "1:850395072418:web:f02d86f49f4f44fc3bd4fb",
+  measurementId: "G-TS6KMQL348"
+};
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 // Initialize Firebase Authentication
